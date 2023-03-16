9888b86e9a00e0c8e3542a970580b17ef5d4ffd0
rahulnavi <127470234+rahulnavi@users.noreply.github.com>
2023-03-16 19:45:29 +0530

    Add files via upload

---
+++ Calculation.java
@@ -0,0 +1,21 @@
+class Calculation
+{
+	public static void main(String args[])
+	{
+		int a=100;
+		int b=50;
+		
+		double add=a+b;
+		double sub=a-b;
+		double div=a/b;
+		double mul=a*b;
+
+		System.out.println("Addition is "+ add);
+		System.out.println("Substraction is "+ sub);
+		System.out.println("Division is "+ div);
+		System.out.println("Multiplication is"+ mul);
+			
+
+		
+	}
+}
