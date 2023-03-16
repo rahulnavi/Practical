fbc31a81041124764d10f66c3cc0c9a106a52889
rahulnavi <127470234+rahulnavi@users.noreply.github.com>
2023-03-16 19:47:38 +0530

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,9 @@
+class Test1
+{
+	public static void main(String args[])
+
+{
+	System.out.println("Hello Guys");
+	System.out.println("My Name Is Rahul And I am using DARKYFPS as my Gamer Tag :)");
+}
+}
