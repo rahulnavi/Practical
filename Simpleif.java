f089b3706e8155e2cd97ddaca98b340ca30c183b
rahulnavi <127470234+rahulnavi@users.noreply.github.com>
2023-03-17 12:02:38 +0530

    Add files via upload

---
+++ Simpleif.java
@@ -0,0 +1,17 @@
+class Simpleif
+{
+	public static void main(String args[])
+	{
+		int x=5;
+		int y=10;
+		if(x+y > 10)
+		{
+			System.out.println("x + y is greater than 10");
+		}
+		else
+		{	
+			System.out.println("x + y is smaller than 10");
+			
+		}
+	}
+}
