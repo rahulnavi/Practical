b724d1b4c0792edabe71740704a2f9e45378f25f
rahulnavi <127470234+rahulnavi@users.noreply.github.com>
2023-03-17 12:13:37 +0530

    Add files via upload

---
+++ Simpif.java
@@ -0,0 +1,11 @@
+class Simpif {
+    public static void main(String args[]) {
+        char a[] = {'R', 'A', 'H', 'U', 'L'};
+        char b[] = {'D', 'A', 'R', 'K', 'Y', 'F', 'P', 'S'};
+        if(a.length > b.length) {
+            System.out.println("a is greater");
+        } else {
+            System.out.println("b is greater");
+        }
+    }
+}
